c8074c44627d11485776c19ad52ab6f092b18222
Cameron Phillips <cameron0505@gmail.com>
2019-07-13 21:31:10 +0100

    Added baz

---
+++ main.py
@@ -0,0 +1,12 @@
+from misc.foo import say_foo
+from misc.bar import say_bar
+from misc.baz import say_baz
+
+def main():
+    say_foo()
+    say_bar()
+    say_baz()
+
+if __name__ == "__main__":
+    main()
+
