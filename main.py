9bebff5c843455be0a1a156a56803a913b1b3505
Cameron Phillips <cameron0505@gmail.com>
2019-07-13 22:22:19 +0100

    Tidy up main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import misc.say
 
-def main():
+def say_all():
     misc.say.foo()
     misc.say.bar()
     misc.say.baz()
+    misc.say.yes()
+
+def main():
+    say_all()
 
 if __name__ == "__main__":
     main()
