2d53137e2ca3e580ca33ff3422661f4740d011cd
Cameron Phillips <cameron0505@gmail.com>
2019-07-13 22:35:03 +0100

    Restored and updated main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import misc.say
+from misc.fib import fib
+
+def say_all():
+    misc.say.foo()
+    misc.say.bar()
+    misc.say.baz()
+    misc.say.yes()
+
+def main():
+    say_all()
+
+    print(list(fib(10)))
+
+if __name__ == "__main__":
+    main()
+
