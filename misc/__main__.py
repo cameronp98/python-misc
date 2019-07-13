f570fc429addb39d7242db44a14e16aeb8e16720
Cameron Phillips <cameron0505@gmail.com>
2019-07-13 21:08:34 +0100

    Added foo+bar functionality

---
+++ misc/__main__.py
@@ -0,0 +1,10 @@
+from foo import say_foo
+from bar import say_bar
+
+def main():
+    say_foo()
+    say_bar()
+
+if __name__ == "__main__":
+    main()
+
---
+++ misc/bar.py
@@ -0,0 +1,3 @@
+def say_bar():
+    print("bar")
+
